ae83ed34e113aaed21e1503312373c4cc78134e1
dans0l <dansolobondo@venturo.co.ke>
2024-01-03 21:45:38 +0300

    Second Commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from blog.models import Post
 
+
 class PostTest(TestCase):
 
     def setUp(self) -> None:
-        self.blog = Post.objects.create(title='django',author='author')
+        self.blog = Post.objects.create(title='django', author='author')
 
     def test_post_model(self) -> None:
-        self.assertTrue(isinstance(self.blog,Post))
-        self.assertEqual(str(self.blog),'django')        
+        self.assertTrue(isinstance(self.blog, Post))
+        self.assertEqual(str(self.blog), 'django')
